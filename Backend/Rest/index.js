d988062f9dee940e156d3de82523d9daafc1e97d
Radu Nicolae <nicolae.radu99@gmail.com>
2021-09-17 11:55:38 +0300

    updated rest api

--- Backend/Rest/index.js
+++ Backend/Rest/index.js
@@ -1,14 +1,24 @@
-const app = require('express')();
+const app = require("express")();
 const PORT = 8080;
+const axios = require('axios');
+const func = require("./functions");
 
-app.listen(
-    PORT,
-    () => console.log('Api is running')
-)
+const result = func.add(4, 4);
+console.log(result)
 
-app.get('/nasa',(req,res)=>{
-    res.status(200).send({
-        nasa:'🚀',
-        marin:'💥'
-    })
-})
+app.listen(PORT, () => console.log("Api is running on Port "+PORT));
+
+
+app.get("/test", async (req, res) => {
+	try {
+		const response = await axios({
+			url: "https://power.larc.nasa.gov/api/temporal/daily/openapi.json",
+			method: "get",
+		});
+		res.status(200).json(response.data);
+	} catch (err) {
+		res.status(500).json({ message: err });
+	}
+});
+
+
