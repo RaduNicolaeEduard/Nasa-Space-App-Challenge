c30b4e27718c53a01b56ec08b4c95ec226587ae2
MarianM00 <marin_puiu99@yahoo.com>
2021-09-20 14:46:14 +0300

    Header&Numaiaud

--- Frontend/SpaceApp/src/app/app.module.ts
+++ Frontend/SpaceApp/src/app/app.module.ts
@@ -11,9 +11,14 @@ import { HttpClientModule } from '@angular/common/http';
 import { VerticalChartComponent } from './vertical-chart/vertical-chart.component';
 import { NgxChartsModule } from '@swimlane/ngx-charts';
 import { DataService } from './data.service';
+import { HeaderComponent } from './header/header.component';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatButtonModule} from '@angular/material/button';
+import {MatIconModule} from '@angular/material/icon';
+
 
 @NgModule({
-  declarations: [AppComponent, MainComponent, VerticalChartComponent],
+  declarations: [AppComponent, MainComponent, VerticalChartComponent, HeaderComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
@@ -22,7 +27,10 @@ import { DataService } from './data.service';
     MatGridListModule,
     HttpClientModule,
     NgxChartsModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    MatButtonModule,
+    MatIconModule
   ],
   providers: [DataService],
   bootstrap: [AppComponent],
